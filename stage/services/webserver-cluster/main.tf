54498aaa225d428ac123092b91ad40d7e403e514
JWeems01 <jonathan@mypay.co.uk>
2022-07-28 14:25:11 +0100

    change file structure

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "serverdevs-terraform-up-and-running-state"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
